ad89594dd17cd0b677337ea28e2538878e6872fa
mrwhiskydk <sein@live.dk>
2019-12-17 20:50:59 +0100

    stack hopefully done

---
+++ AStarGroceryStore/AStarGroceryStore/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarGroceryStore
+{
+    class Node<T>
+    {
+        T value;
+
+        public Node<T> Previous { get; set; }
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+
+        public T GetValue()
+        {
+            if (value == null)
+            {
+                value = default(T);
+            }
+            return value;
+        }
+    }
+}
