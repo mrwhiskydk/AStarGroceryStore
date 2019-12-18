1fdb7c6857542c539f579fe1bf776af83f4be8a9
Morten Skov <morten_b_s@hotmail.com>
2019-12-18 22:04:03 +0100

    dwdw

--- AStarGroceryStore/AStarGroceryStore/ShoppingList.cs
+++ AStarGroceryStore/AStarGroceryStore/ShoppingList.cs
@@ -14,11 +14,61 @@ namespace AStarGroceryStore
 
         public ShoppingList()
         {
+            MyList<Department> tmpDepartments = new MyList<Department>();
+
+            tmpDepartments.Add(new Baker());
+            tmpDepartments.Add(new Fruit());
+            tmpDepartments.Add(new Butcher());
+
+            ShuffleShoppingList(tmpDepartments); // we're shuffling all 3 departments randomly, and then removes the last one, so AI has 2 departments to visit
+            ShuffleShoppintItems(Departments); // we're shuffling all 3 items randomly, for each department to visit
+        }
+
+        private void ShuffleShoppintItems(MyList<Department> departments)
+        {
+            foreach(Department dep in departments)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Shuffles the 3 departements randomly, from temporary list, through array, to actual list of departments 
+        /// </summary>
+        /// <param name="departments"></param>
+        private void ShuffleShoppingList(MyList<Department> departments)
+        {
+            Department[] tmpArray = new Department[departments.Count];
+
+            foreach(Department dep in departments)
+            {
+                for (int i = 0; i < tmpArray.Length; i++)
+                {
+                    if(tmpArray[i] == null)
+                    {
+                        tmpArray[i] = dep;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = tmpArray.Length - 1; i > 0; i--)
+            {
+                Random random = new Random();
+                int rnd = random.Next(0, i);
+
+                Department tmp = tmpArray[i];
+
+                tmpArray[i] = tmpArray[rnd];
+                tmpArray[rnd] = tmp;
+            }
+
             Departments = new MyList<Department>();
 
-            Departments.Add(new Baker());
-            Departments.Add(new Fruit());
-            Departments.Add(new Butcher());
+            for (int i = 0; i < 2; i++)
+            {
+                Departments.Add(tmpArray[i]);
+            }
         }
     }
 }
