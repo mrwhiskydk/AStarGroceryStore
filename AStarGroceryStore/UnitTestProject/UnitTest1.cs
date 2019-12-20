99d1a20910b9dfcfed2128dc07ee53777a7117ff
mrwhiskydk <sein@live.dk>
2019-12-20 09:56:37 +0100

    jhjkhkl

--- AStarGroceryStore/UnitTestProject/UnitTest1.cs
+++ AStarGroceryStore/UnitTestProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AStarGroceryStore;
 
 namespace UnitTestProject
 {
@@ -7,8 +8,58 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MyStack_Add()
         {
+            MyStack<int> myStack = new MyStack<int>();
+            int expected = 7;
+            int actual;
+
+            myStack.Push(expected);
+            actual = myStack.Pop();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MyStack_Remove()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            int expected = 7;
+            int actual;
+            int expectedSize = 0;
+            int actualSize;
+
+            myStack.Push(expected);
+            actual = myStack.Pop();
+
+            actualSize = myStack.Count;
+
+            //check removed object
+            Assert.AreEqual(expected, actual);
+
+            //check size
+            Assert.AreEqual(expectedSize, actualSize);
+        }
+
+        [TestMethod]
+        public void MyStack_Peek()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            int expected = 7;
+            int actual;
+            int expectedSize = 1;
+            int actualSize;
+
+            myStack.Push(expected);
+            actual = myStack.Peek();
+
+            actualSize = myStack.Count;
+
+            //check peeked object
+            Assert.AreEqual(expected, actual);
+
+            //check size
+            Assert.AreEqual(expectedSize, actualSize);
         }
     }
 }
